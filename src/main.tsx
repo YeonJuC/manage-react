497d97706ff3b087c7cf2820277064cc211630a2
carplay1110 <carplay1110@naver.com>
2026-01-19 15:19:37 +0900

    Browser Router

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
 import "react-calendar/dist/Calendar.css";
 
-/**
- * GitHub Pages 배포 시 repo명이 base 경로가 됨.
- * 예) https://username.github.io/manage  -> base = "/manage/"
- * 로컬에서는 "/"로 동작.
- *
- * 아래처럼 import.meta.env.BASE_URL 쓰면
- * vite.config.ts의 base 값과 자동으로 맞춰짐.
- */
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
+
+
+
 
